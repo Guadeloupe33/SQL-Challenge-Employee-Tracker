e0fb724eb5237cd27d41076fc2299d4c26caef4c
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-17 17:57:28 -0400

    added department.js

---
+++ models/department.js
@@ -0,0 +1,13 @@
+const connection = require('../db/connection');
+
+class Department {
+  static getAllDepartments() {
+    return getAllDepartments();
+  }
+
+  static addDepartment(name) {
+    return addDepartment(name);
+  }
+}
+
+module.exports = Department;
