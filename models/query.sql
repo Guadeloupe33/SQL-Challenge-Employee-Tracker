06bc80fc345028353163d12e697ecbe6ebcb6dc2
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-19 19:19:55 -0400

    added some addtional funactionality to the project

---
+++ models/query.sql
@@ -0,0 +1,1 @@
+SELECT role.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, employee.manager_id AS manager from employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id;
