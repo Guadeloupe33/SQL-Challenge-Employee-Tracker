3b31dadfab682da5e4ac50e8ca1f7ee2fb387f30
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-19 19:15:48 -0400

    made some changes to the database

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE CompanyManagementDB;
-DROP DATABASE IF EXISTS CompanyManagementDB;
+CREATE DATABASE CompanyManagement_DB;
+DROP DATABASE IF EXISTS CompanyManagement_DB;
 
 
 USE CompanyManagementDB;
@@ -7,4 +7,22 @@ USE CompanyManagementDB;
 CREATE TABLE department (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
+);
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
 );
